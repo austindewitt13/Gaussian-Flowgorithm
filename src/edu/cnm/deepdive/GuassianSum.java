63a8045bab83a6b61a5ec82f0e0278716547d40f
austindewitt13 <sdewitt2@cnm.edu>
2019-05-21 10:32:42 -0600

    third commit added while

--- src/edu/cnm/deepdive/GuassianSum.java
+++ src/edu/cnm/deepdive/GuassianSum.java
@@ -5,28 +5,32 @@ import java.util.Scanner;
 
 public class GuassianSum {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
 
-        int upperLimit;
-        
-        upperLimit = -1;
-        long sum;
-        
-        while (upperLimit < 1 || upperLimit > 10000000) {
-            System.out.println("input a value between 1 and 1000000");
-            upperLimit = input.nextInt();
-            if (upperLimit > 1) {
-            } else {
-                System.out.println("Not a correct value, please try again.");
-            }
-        }
-        sum = 0;
-        int i;
-        
-        for (i = 1; i <= upperLimit; i++) {
-            sum = (long) upperLimit * (upperLimit + 1)/2;
-        }
-        System.out.println(sum);
+    int upperLimit;
+
+    upperLimit = -1;
+    long sum;
+
+    while (upperLimit < 1 || upperLimit > 10000000) {
+      System.out.println("input a value between 1 and 1000000");
+      while (!input.hasNextInt()) {
+        input.next();
+        System.out.println("not valid integer");
+      }
+      upperLimit = input.nextInt();
+      if (upperLimit > 1) {
+      } else {
+        System.out.println("Not a correct value, please try again.");
+      }
     }
+    sum = 0;
+    int i;
+
+    for (i = 1; i <= upperLimit; i++) {
+      sum = (long) upperLimit * (upperLimit + 1) / 2;
+    }
+    System.out.println(sum);
+  }
 }
